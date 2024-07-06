2e36f0d01fd30209c0f57cfb3ee39862243cfdb2
Mitko Mitev <onesible@gmail.com>
2024-07-06 16:57:27 +0300

    updated admin

--- django_models_relations_exercise/main_app/admin.py
+++ django_models_relations_exercise/main_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from main_app.models import Car
+
 
 # Register your models here.
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['model', 'year', 'owner', 'car_details']
+
+    @staticmethod
+    def car_details(car: Car):
+        try:
+            owner = car.owner.name
+        except AttributeError:
+            owner = 'No owner'
+
+        try:
+            registration_number = car.registration.registration_number
+        except AttributeError:
+            registration_number = 'No registration number'
+
+        return f"Owner: {owner}, Registration: {registration_number}"
+
+    car_details.short_description = 'Car Details'
