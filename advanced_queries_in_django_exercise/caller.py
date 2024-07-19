8a3bb00471c06a137ef459cc2a03805b3400305e
Mitko Mitev <onesible@gmail.com>
2024-07-19 20:16:15 +0300

    added solution

---
+++ advanced_queries_in_django_exercise/caller.py
@@ -0,0 +1,14 @@
+import os
+from datetime import date
+
+import django
+
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+from main_app.models import (RealEstateListing, VideoGame, BillingInfo, Invoice,
+                             Programmer, Project, Technology, Task, Exercise)
